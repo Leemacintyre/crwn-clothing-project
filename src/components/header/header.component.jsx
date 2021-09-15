6acf49ff0ff158701d0090e579e7736e79aaea7a
Leemacintyre <64503108+Leemacintyre@users.noreply.github.com>
2021-09-15 06:58:54 +0100

    created user and cart sagas for sign in, sign up and sign in with google

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect';
 
-import { auth } from '../../firebase/firebase.utils'
-
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import "./header.styles.scss"
 import { HeaderContainer, LogoContainer, OptionLink, OptionsContainer } from './header.styles'
@@ -12,9 +10,10 @@ import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selector';
+import { signOutStart } from '../../redux/user/user.actions';
 
 
-const Header = ({ currentUser, hidden }) => {
+const Header = ({ currentUser, hidden, signOutStart }) => {
     return (
         <HeaderContainer>
             <LogoContainer to="/">
@@ -29,7 +28,7 @@ const Header = ({ currentUser, hidden }) => {
                 </OptionLink>
                 {
                     currentUser ?
-                        <OptionLink as={'div'} onClick={() => auth.signOut()}>SIGN OUT</OptionLink>
+                        <OptionLink as={'div'} onClick={signOutStart}>SIGN OUT</OptionLink>
                         :
                         <OptionLink to='/signin'>SIGN IN</OptionLink>
                 }
@@ -47,9 +46,8 @@ const mapStateToProps = createStructuredSelector({
     hidden: selectCartHidden
 });
 
-const mapDispatchToProps = {
-
-}
-
+const mapDispatchToProps = (dispatch) => ({
+    signOutStart: () => dispatch(signOutStart())
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
