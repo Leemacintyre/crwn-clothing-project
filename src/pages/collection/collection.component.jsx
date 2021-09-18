881e0e103ed0081d5c06382085ed32739daf8d65
Leemacintyre <64503108+Leemacintyre@users.noreply.github.com>
2021-09-18 06:24:14 +0100

    cart dropdown and collections page converted to hooks

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 import CollectionItem from '../../components/collection-item/collection-item.component'
 
@@ -7,7 +8,10 @@ import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import './collection.styles.scss'
 
-const CategoryPage = ({ collection }) => {
+const CategoryPage = () => {
+    const { collectionId } = useParams();
+    const collection = useSelector(selectCollection(collectionId))
+
     const { title, items } = collection
     console.log(collection)
 
@@ -25,8 +29,4 @@ const CategoryPage = ({ collection }) => {
     );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-    collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
-
-export default connect(mapStateToProps)(CategoryPage);
+export default CategoryPage;
